d342cb91d4958f86416d3e3820f9ba80138300d4
andiaqsha99 <andi77aqsha@gmail.com>
2019-11-24 17:48:53 +0700

    change base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,11 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-<<<<<<< HEAD
 $config['base_url'] = 'http://localhost/WebSepatu';
-=======
-$config['base_url'] = 'http://localhost/Kelompok10';
->>>>>>> f394bca673542b4c53de11298ee905dcdd423f5f
+
+
 
 /*
 |--------------------------------------------------------------------------
