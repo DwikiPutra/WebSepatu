1db481c7d58c46cc7596a41781e29c33e5034dad
andiaqsha99 <andi77aqsha@gmail.com>
2019-11-25 09:02:16 +0700

    add register_view

---
+++ application/views/register_view.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<head>
+    <?php $this->load->view("_partials/header.php") ?>
+</head>
+
+<body>
+
+    <?php $this->load->view("_partials/navbar.php") ?>
+
+	<!-- Start Banner Area -->
+	<section class="banner-area organic-breadcrumb">
+		<div class="container">
+			<div class="breadcrumb-banner d-flex flex-wrap align-items-center justify-content-end">
+				<div class="col-first">
+					<h1>Register</h1>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End Banner Area -->
+
+	<!--================Login Box Area =================-->
+	<section class="login_box_area section_gap">
+		<div class="container">
+			<div class="row">
+                <div class="col-lg-3">
+					
+				</div>
+				<div class="col-lg-6">
+					<div class="login_form_inner">
+						<h3>Register</h3>
+						<form class="row login_form" action="<?php echo base_url('login/form')?>" method="post" id="contactForm" novalidate="novalidate">
+							<div class="col-md-12 form-group">
+								<input type="text" class="form-control" id="name" name="name" placeholder="Username" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'">
+							</div>
+                            <div class="col-md-12 form-group">
+								<input type="text" class="form-control" id="name" name="email" placeholder="Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'">
+							</div>
+							<div class="col-md-12 form-group">
+								<input type="password" class="form-control" id="name" name="password" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'">
+							</div>
+							<div class="col-md-12 form-group">
+                                <input type="password" class="form-control" id="name" name="password" placeholder="Confirm Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'">
+							</div>
+							<div class="col-md-12 form-group">
+								<button type="submit" value="submit" class="primary-btn">Create</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================End Login Box Area =================-->
+
+	<?php $this->load->view("_partials/footer.php")?>
+
+	<?php $this->load->view("_partials/js.php")?>
+</body>
+
+</html>
