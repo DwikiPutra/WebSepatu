4e7918e48ba9a1c554e8180acca28ed31cc91fe3
105217011-Dwiki <dwikiputera41@yahoo.co.id>
2019-11-25 08:53:00 +0700

    Delete Nav Href

--- application/views/shop/cart_view.php
+++ application/views/shop/cart_view.php
@@ -15,10 +15,6 @@
             <div class="breadcrumb-banner d-flex flex-wrap align-items-center justify-content-end">
                 <div class="col-first">
                     <h1>Shopping Cart</h1>
-                    <nav class="d-flex align-items-center">
-                        <a href="index.html">Home<span class="lnr lnr-arrow-right"></span></a>
-                        <a href="category.html">Cart</a>
-                    </nav>
                 </div>
             </div>
         </div>
