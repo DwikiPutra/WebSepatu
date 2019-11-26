1cf3430e00c74bd42602e7fad50a00594ea634e5
105217011-Dwiki <dwikiputera41@yahoo.co.id>
2019-11-26 11:50:22 +0700

    Akhirnya Cart nya bisa

--- application/views/shop/cart_view.php
+++ application/views/shop/cart_view.php
@@ -80,11 +80,11 @@
                                 </td>
                                 <td>
                                     <div class="product_count">
-                                        <input type="text" name="qty" id="sst" maxlength="12" value="3" title="Quantity:"
+                                        <input type="text" name="qty" id="sstt" maxlength="12" value="3" title="Quantity:"
                                             class="input-text qty">
-                                        <button onclick="var result = document.getElementById('sst'); var sst = result.value; if( !isNaN( sst )) result.value++;return false;"
+                                        <button onclick="var result = document.getElementById('sstt'); var sstt = result.value; if( !isNaN( sstt )) result.value++;return false;"
                                             class="increase items-count" type="button"><i class="lnr lnr-chevron-up"></i></button>
-                                        <button onclick="var result = document.getElementById('sst'); var sst = result.value; if( !isNaN( sst ) &amp;&amp; sst > 0 ) result.value--;return false;"
+                                        <button onclick="var result = document.getElementById('sstt'); var sstt = result.value; if( !isNaN( sstt ) &amp;&amp; sstt > 0 ) result.value--;return false;"
                                             class="reduced items-count" type="button"><i class="lnr lnr-chevron-down"></i></button>
                                     </div>
                                 </td>
